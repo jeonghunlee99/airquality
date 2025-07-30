f20a2ca15c852397659debaa3b088316f89f4a57
main <zao3662@naver.comgit config --global user.email zao3662@naver.com>
2025-07-30 18:40:59 +0900

    tm좌표 페이지

---
+++ lib/utils/tm_converter.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:proj4dart/proj4dart.dart';
+
+import '../airquality/air_quality_data.dart';
+
+final wgs84 = Projection.get('EPSG:4326')!;
+final tmMid = Projection.add(
+  'EPSG:2097',
+  '+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=GRS80 +units=m +no_defs',
+);
+
+Point convertLatLngToTM(double lng, double lat) {
+  final input = Point(x: lng, y: lat);
+  return wgs84.transform(tmMid, input);
+}
+
+void setCoordinates(WidgetRef ref, double lng, double lat) {
+  final tmPoint = convertLatLngToTM(lng, lat);
+
+  ref.read(tmXProvider.notifier).state = double.parse(
+    tmPoint.x.toStringAsFixed(2),
+  );
+  ref.read(tmYProvider.notifier).state = double.parse(
+    tmPoint.y.toStringAsFixed(2),
+  );
+}
